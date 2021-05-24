8c3e407a30660094597e6a761f2df75db12075d5
mbish08 <mdrew8@gmail.com>
2021-05-24 16:26:16 -0700

    Done.

---
+++ src/Login.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <input type="text" name="username" placeholder="Username" />
+                        <label htmlFor="username">Username</label>
+                    </div>
+                    <div>
+                        <input type="password" name="password" placeholder="Password" />
+                        <label htmlFor="password">Password</label>
+                    </div>
+                    <input type="submit" value="Login" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About.js'
+import Login from './Login.js'
+import Navbar from './Navbar.js'
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
